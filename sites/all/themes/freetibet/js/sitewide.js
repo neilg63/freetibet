19884f8cae44bb109db1067da8329b555556e773
root <root@s15420365.onlinehome-server.info>
2015-05-27 14:43:08 +0100

    ongoing dev

--- sites/all/themes/freetibet/js/sitewide.js
+++ sites/all/themes/freetibet/js/sitewide.js
@@ -1,5 +1,57 @@
 (function($) {
 
+	window.viewportSize = {};
+
+		window.viewportSize.getHeight = function () {
+			return getSize("Height");
+		};
+
+		window.viewportSize.getWidth = function () {
+			return getSize("Width");
+		};
+
+		var getSize = function (Name) {
+			var size;
+			var name = Name.toLowerCase();
+			var document = window.document;
+			var documentElement = document.documentElement;
+			if (window["inner" + Name] === undefined) {
+				// IE6 & IE7 don't have window.innerWidth or innerHeight
+				size = documentElement["client" + Name];
+			}
+			else if (window["inner" + Name] != documentElement["client" + Name]) {
+				// WebKit doesn't include scrollbars while calculating viewport size so we have to get fancy
+
+				// Insert markup to test if a media query will match document.doumentElement["client" + Name]
+				var bodyElement = document.createElement("body");
+				bodyElement.id = "vpw-test-b";
+				bodyElement.style.cssText = "overflow:scroll";
+				var divElement = document.createElement("div");
+				divElement.id = "vpw-test-d";
+				divElement.style.cssText = "position:absolute;top:-1000px";
+				// Getting specific on the CSS selector so it won't get overridden easily
+				divElement.innerHTML = "<style>@media(" + name + ":" + documentElement["client" + Name] + "px){body#vpw-test-b div#vpw-test-d{" + name + ":7px!important}}</style>";
+				bodyElement.appendChild(divElement);
+				documentElement.insertBefore(bodyElement, document.head);
+
+				if (divElement["offset" + Name] == 7) {
+					// Media query matches document.documentElement["client" + Name]
+					size = documentElement["client" + Name];
+				}
+				else {
+					// Media query didn't match, use window["inner" + Name]
+					size = window["inner" + Name];
+				}
+				// Cleanup
+				documentElement.removeChild(bodyElement);
+			}
+			else {
+				// Default to use window["inner" + Name]
+				size = window["inner" + Name];
+			}
+			return size;
+		};
+
 	$.fn.fitIframes = function() {
 	   var items = this, numItems = items.length,item, i=0,w,h,fr,fw,fh,ar;
 	   if (numItems>0) {
@@ -31,9 +83,14 @@
 	Drupal.ft = {
 			
 		resizeIframes: function() {
-			var fc = Drupal.settings.ft.iframes;
+			var s = Drupal.settings.ft, fc = s.iframes;
+			s.width = window.viewportSize.getWidth();
 			if (fc.length>0) {
 				fc.fitIframes();
+			}
+			if (s.width >= s.desktopWidth) {
+				s.header.removeAttr('style');
+			  s.b.removeClass('menu-expanded');
 			}
 		},
 
@@ -59,11 +116,17 @@
 			$('.menu-toggle').on('click', function(e){
 				e.preventDefault();
 				e.stopImmediatePropagation();
-				var s = Drupal.settings.ft;
-				if (s.b.hasClass('menu-expanded')) {
-					s.b.removeClass('menu-expanded');
-				} else {
-					s.b.addClass('menu-expanded');
+				var s = Drupal.settings.ft,h;
+				if (s.width <= s.desktopWidth) {	
+					if (s.b.hasClass('menu-expanded')) {
+						h = s.logo.height();
+						s.header.css('height', h + 'px');
+						s.b.removeClass('menu-expanded');
+					} else {
+						h = (s.menuItems.first().height() * s.numMenuItems) + (s.sbox.height() * 1.5) + s.logo.height();
+						s.header.css('height', h + 'px');
+						s.b.addClass('menu-expanded');
+					}
 				}
 			});
 		},
@@ -71,8 +134,15 @@
 		init: function() {
 			Drupal.settings.ft = {};
 			var s = Drupal.settings.ft;
+			s.desktopWidth = 800;
+			s.width = window.viewportSize.getWidth();
 			s.iframes = $('.file-video, .file-audio-soundcloud');
 			s.b = $('body');
+			s.sbox =  $('#block-search-form');
+			s.header = $('#header');
+			s.menuItems = s.header.find('.region-header nav li');
+			s.numMenuItems = s.menuItems.length;
+			s.logo = $('#logo');
 			this.resizeIframes();
 			//this.highlightPlatform3();
 			this.shareThisLabelClick();
