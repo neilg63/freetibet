c9a57b505de81ce397fd548b46f2c0b25e818a61
root <root@s15420365.onlinehome-server.info>
2015-05-28 12:58:57 +0100

    ongoing dev

--- sites/all/themes/freetibet/templates/page.tpl.php
+++ sites/all/themes/freetibet/templates/page.tpl.php
@@ -8,7 +8,6 @@
  */
 
 ?>
-
 <div id="page-wrapper">
 
   <header class="header" id="header" role="banner">
@@ -16,23 +15,13 @@
     <?php if ($logo): ?>
       <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><span class="alt-text"><?php print t('Home'); ?></span></a>
     <?php endif; ?>
-
-    <?php if ($site_name || $site_slogan): ?>
-      <div class="header__name-and-slogan" id="name-and-slogan">
-        <?php if ($site_name): ?>
-          <h1 class="header__site-name" id="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
-          </h1>
-        <?php endif; ?>
+		<?php print render($page['top']); ?>
 
         <?php if ($site_slogan): ?>
           <div class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
         <?php endif; ?>
-      </div>
-    <?php endif; ?>
-
-    <?php print render($page['header']); ?>
-		
+    
+		<?php print render($page['header']); ?>
   </header>
 
   <div id="main">
